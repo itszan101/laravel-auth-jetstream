acd2eaa54f76b5a943dc5534b1020ad7b4c49cdc
itszan101 <itszan29@gmail.com>
2023-04-17 14:20:58 +0700

    add form kendaraan

---
+++ resources/views/livewire/create-kendaraan.blade.php
@@ -0,0 +1,83 @@
+<div id="form-create">
+    <x-jet-form-section :submit="$action" class="mb-4">
+        <x-slot name="title">
+            {{ __('Formulir data kendaraan') }}
+        </x-slot>
+
+        <x-slot name="description">
+            {{ __('Lengkapi data berikut dan submit untuk membuat data kendaraan baru') }}
+        </x-slot>
+
+        <x-slot name="form">
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('NIK') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="name" value="{{ __('Nama Pemilik') }}" />
+                <small>Nama Lengkap Akun</small>
+                <x-jet-input id="name" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.name" />
+                <x-jet-input-error for="user.name" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('No. Kendaraan') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('No. Rangka') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('No. Mesin') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('Kapasitas Mesin') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('Foto Kendaraan') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('Nama Kendaraan') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('Tahun Kendaraan') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            
+        </x-slot>
+
+        <x-slot name="actions">
+            <x-jet-action-message class="mr-3" on="saved">
+                {{ __($button['submit_response']) }}
+            </x-jet-action-message>
+
+            <x-jet-button>
+                {{ __($button['submit_text']) }}
+            </x-jet-button>
+        </x-slot>
+    </x-jet-form-section>
+
+    <x-notify-message on="saved" type="success" :message="__($button['submit_response_notyf'])" />
+</div>
